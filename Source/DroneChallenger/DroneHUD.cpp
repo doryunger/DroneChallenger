1adf5b42a6ab246ae4c974490b53c3dbc7b242af
Dor Yunger <dor.yunger@gmail.com>
2026-05-31 10:59:36 +0200

    Feat: position HUD widgets from C++ using SetDesiredSizeInViewport
    PFD: 22% x 28% of viewport, centered horizontally, lower third of screen.
    MiniMap: 240x240, top-right corner with 20px margin.
    TrackingDisplay: top-left corner at (20,20).
    Crosshair: full viewport (no size constraint, draws at widget center).
    Viewport size sampled at BeginPlay for relative positioning.

--- Source/DroneChallenger/DroneHUD.cpp
+++ Source/DroneChallenger/DroneHUD.cpp
@@ -7,6 +7,7 @@
 #include "DroneActor.h"
 #include "TargetPawn.h"
 #include "Kismet/GameplayStatics.h"
+#include "Engine/GameViewportClient.h"
 
 void ADroneHUD::BeginPlay()
 {
@@ -14,6 +15,10 @@ void ADroneHUD::BeginPlay()
 
     ADroneActor* Drone  = Cast<ADroneActor>(UGameplayStatics::GetActorOfClass(GetWorld(), ADroneActor::StaticClass()));
     ATargetPawn* Target = Cast<ATargetPawn>(UGameplayStatics::GetActorOfClass(GetWorld(), ATargetPawn::StaticClass()));
+
+    FVector2D VP(1920.f, 1080.f);
+    if (GetWorld() && GetWorld()->GetGameViewport())
+        GetWorld()->GetGameViewport()->GetViewportSize(VP);
 
     if (BTDisplayWidgetClass)
     {
@@ -24,24 +29,46 @@ void ADroneHUD::BeginPlay()
     if (TrackingWidgetClass)
     {
         TrackingWidget = CreateWidget<UDroneTrackingWidget>(GetWorld(), TrackingWidgetClass);
-        if (TrackingWidget) { TrackingWidget->Init(Drone, Target); TrackingWidget->AddToViewport(); }
+        if (TrackingWidget)
+        {
+            TrackingWidget->Init(Drone, Target);
+            TrackingWidget->AddToViewport();
+            TrackingWidget->SetPositionInViewport(FVector2D(20.f, 20.f));
+        }
     }
 
     if (MiniMapWidgetClass)
     {
         MiniMapWidget = CreateWidget<UDroneMiniMapWidget>(GetWorld(), MiniMapWidgetClass);
-        if (MiniMapWidget) { MiniMapWidget->Init(Drone, Target); MiniMapWidget->AddToViewport(); }
+        if (MiniMapWidget)
+        {
+            MiniMapWidget->Init(Drone, Target);
+            MiniMapWidget->AddToViewport();
+            MiniMapWidget->SetDesiredSizeInViewport(FVector2D(240.f, 240.f));
+            MiniMapWidget->SetPositionInViewport(FVector2D(VP.X - 260.f, 20.f));
+        }
     }
 
     if (PFDWidgetClass)
     {
         PFDWidget = CreateWidget<UDronePFDWidget>(GetWorld(), PFDWidgetClass);
-        if (PFDWidget) { PFDWidget->Init(Drone); PFDWidget->AddToViewport(); }
+        if (PFDWidget)
+        {
+            PFDWidget->Init(Drone);
+            PFDWidget->AddToViewport();
+            PFDWidget->SetDesiredSizeInViewport(FVector2D(VP.X * 0.22f, VP.Y * 0.28f));
+            PFDWidget->SetPositionInViewport(
+                FVector2D((VP.X - VP.X * 0.22f) * 0.5f, VP.Y * 0.62f));
+        }
     }
 
     if (CrosshairWidgetClass)
     {
         CrosshairWidget = CreateWidget<UDroneCrosshairWidget>(GetWorld(), CrosshairWidgetClass);
-        if (CrosshairWidget) { CrosshairWidget->Init(Drone); CrosshairWidget->AddToViewport(); }
+        if (CrosshairWidget)
+        {
+            CrosshairWidget->Init(Drone);
+            CrosshairWidget->AddToViewport();
+        }
     }
 }
